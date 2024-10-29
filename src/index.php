71304541cbaa73e85291efdc48d58991b12d2afc
DefinitelyNotJay <jjbrathers3@gmail.com>
2024-10-29 18:15:13 +0700

    change

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
     function connectDatabase($host, $username, $password, $database)
     {
         $connection = mysqli_connect($host, $username, $password, $database);
-
+        
         if (!$connection) {
             die("Connection failed: " . mysqli_connect_error());
         }
